fc1b6e12dc2d223af6bc1175891ac7245e340d7e
SuHun Han <ssut@ssut.me>
2017-02-16 23:38:11 +0900

    Add monkeypatch feature

---
+++ publicdns/monkey.py
@@ -0,0 +1,13 @@
+from publicdns.client import PublicDNS
+
+client = PublicDNS()
+def _gethostbyname(hostname):
+    try:
+        return client.resolve(hostname)[0]
+    except:
+        return '0.0.0.0'
+
+def patch_socket():
+    module = __import__('socket')
+    setattr(module, 'gethostbyname', _gethostbyname)
+
--- tests/test_gethostbyname.py
+++ tests/test_gethostbyname.py
@@ -1,1 +1,7 @@
 
+def test_gethostbyname():
+    from publicdns.monkey import patch_socket
+    patch_socket()
+    import socket
+    assert socket.gethostbyname('google-public-dns-a.google.com')
+
