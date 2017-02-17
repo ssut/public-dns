a0619b422792ed65b2411ed0fce329f850826176
SuHun Han <ssut@ssut.me>
2017-02-17 22:20:18 +0900

    Add more tests for client

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,11 +1,10 @@
 import pytest
-import ipaddress
 
-from publicdns.client import PublicDNS
-
-def test_client():
-    client = PublicDNS()
+def test_client_resolve_a(client):
     resp = client.resolve('google-public-dns-a.google.com')
-    assert set(resp).intersection([ipaddress.IPv4Address('8.8.8.8')])
+    assert set(resp).intersection(['8.8.8.8'])
 
+def test_client_resolve_ptr(client):
+    resp = client.resolve('8.8.8.8', type='PTR')
+    assert set(resp).intersection(['google-public-dns-a.google.com.'])
 
